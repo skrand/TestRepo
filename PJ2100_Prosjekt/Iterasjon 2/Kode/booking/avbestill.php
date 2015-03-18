133f7b4332f1743aa4b1bfa090d5e2b40a06c9d0
TordTrøen <assessedgrub@gmail.com>
2015-03-18 16:05:34 +0100

    La til avbestillings redirect

--- PJ2100_Prosjekt/Iterasjon 2/Kode/booking/avbestill.php
+++ PJ2100_Prosjekt/Iterasjon 2/Kode/booking/avbestill.php
@@ -5,13 +5,6 @@ $roomId = $_GET['roomid'];
 $date = $_GET['date'];
 $time = $_GET['time'];
 
-echo "ROOM: " . $roomId;
-echo "<br />";
-echo "DATE: " . $date;
-echo "<br />";
-echo "TIME: " . $time;
-echo "<br />";
-
 $sql = $db->prepare("DELETE FROM LeieAvRom WHERE RomId LIKE :roomId AND Dato LIKE :date AND Tidspunkt LIKE :time;");
 $sql->setFetchMode(PDO::FETCH_OBJ);
 $sql->execute(array(
@@ -19,5 +12,6 @@ $sql->execute(array(
     'date' => $date,
     'time' => $time
 ));
-//echo $sql->fetch()['Tidspunkt'];
-var_dump($sql->fetch());
+
+header("Location: index.php");
+die();
