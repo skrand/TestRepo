add7300972a873d09b56de238c2d3a02b31c49a1
TordTrøen <assessedgrub@gmail.com>
2015-03-19 16:03:25 +0100

    Fjernet v2 mappe

--- PJ2100_Prosjekt/Iterasjon 2/KodeV2/booking/header.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<header>";
-echo "<a href='../index.php'><img src='../images/logo4.png'/></a>";
-echo "</header>";
--- PJ2100_Prosjekt/Iterasjon 2/KodeV2/booking/verifysession.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Used to verify if a user is logged in
-
-// Redirect to main
-if (!isValidSession())
-{
-    header("Location: ../index.php?badlogin");
-    die();
-}
